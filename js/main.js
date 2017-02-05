f9a2eb61f7dd4a6395b29ab0edfcbb929a68afb5
David Hildering <david@purplebean.nl>
2017-02-05 22:41:04 +0100

    final commit - add css animations and complete form with ajax post

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
-console.log('main');
+//form submit Ajax
+
+$(function() {
+    $("div#submit").click(function(){
+        var email = $("#email").val();
+        $.ajax({
+            type: "POST",
+            url: "",
+            data: email,
+            success: function(data){
+                if(email.trim()=='') {
+                    showError();
+                } else {
+                    showSucces();
+                    console.log ('succes! your ' + email + ' has been sent to us' );
+                }
+            },
+            error: function(){
+                console.log ('failure');
+            }
+        });
+    });
+});
+
+function showSucces() {
+    $('#form-open').addClass('hide');
+    $('#form-succes').removeClass('hide');
+
+}
+function showError() {
+    $('.form-error').removeClass('hide');
+}
--- js/plugins.js
+++ js/plugins.js
@@ -1,12 +1,13 @@
+$(document).ready(function () {
 
-$(document).ready(function() {
-    //svg color ccnverter
-    $('img[src$=".svg"]').each(function() {
+    //svg color converter
+
+    $('img[src$=".svg"]').each(function () {
         var $img = jQuery(this);
         var imgURL = $img.attr('src');
         var attributes = $img.prop("attributes");
 
-        $.get(imgURL, function(data) {
+        $.get(imgURL, function (data) {
             // Get the SVG tag, ignore the rest
             var $svg = jQuery(data).find('svg');
 
@@ -14,7 +15,7 @@ $(document).ready(function() {
             $svg = $svg.removeAttr('xmlns:a');
 
             // Loop through IMG attributes and apply on SVG
-            $.each(attributes, function() {
+            $.each(attributes, function () {
                 $svg.attr(this.name, this.value);
             });
 
@@ -22,12 +23,14 @@ $(document).ready(function() {
             $img.replaceWith($svg);
         }, 'xml');
     });
-//smmoth scrolling
-    $(function() {
-        $('a[href*="#"]:not([href="#"])').click(function() {
-            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+//smooth scrolling
+
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
                 var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
                 if (target.length) {
                     $('html, body').animate({
                         scrollTop: target.offset().top
@@ -37,4 +40,30 @@ $(document).ready(function() {
             }
         });
     });
+
+
+// fade in content
+
+    $(function () {
+        $(window).scroll(function () {
+
+
+            $('.fadeInBlock').each(function (i) {
+
+                var bottom_of_object = $(this).position().top + $(this).outerHeight();
+                var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+                /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
+                bottom_of_window = bottom_of_window + 100;
+
+                if (bottom_of_window > bottom_of_object) {
+
+                    $(this).animate({'opacity': '1', 'margin-top': '-10px'}, 1000);
+
+                }
+            });
+
+        });
+    });
+
 });
